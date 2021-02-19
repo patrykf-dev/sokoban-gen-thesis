6f56306a4d6e4e877df59ec5519bbd3967512b8a
gsrmeen <patryk.gsrmeen@gmail.com>
2021-02-19 18:59:36 +0100

    Add dotnet framework repo

---
+++ SokobanLevelsGenerator/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Common;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var level = new SokobanLevel();
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ SokobanLevelsGenerator/LevelGenerator/SokobanLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Common
+{
+    public class SokobanLevel
+    {
+        public SokobanLevel()
+        {
+        }
+    }
+}
